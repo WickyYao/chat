ee0064a8bd8c856dd422fa360453e73efa6a5f78
Ubuntu <wickyblog@gmail.com>
2014-06-03 13:52:01 +0000

    initialize

---
+++ chatter2/static/chatter.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    WEB_SOCKET_SWF_LOCATION = "/static/WebSocketMain.swf";
+    WEB_SOCKET_DEBUG = true;
+
+    // connect to the websocket
+    var socket = io.connect('/chat');
+
+    $(window).bind("beforeunload", function() {
+        socket.disconnect();
+    });
+
+    // Listen for the event "chat" and add the content to the log
+    socket.on("chat", function(time, msg) {
+        $("#chatlog").append(time + ' - ' + msg + "<br />");
+        $("#chatlog").stop().animate({scrollTop:$("#chatlog")[0].scrollHeight},1000)
+    });
+
+    socket.on("user_disconnect", function() {
+        $("#chatlog").append("user disconnected" + "<br />");
+    });
+
+    socket.on("user_connect", function() {
+        $("#chatlog").append("user connected" + "<br />");
+    });
+
+    socket.on("writing", function(val) {
+        if (val){
+            document.getElementById('writing').innerHTML = 'Writing... '+val
+        }
+        else{
+            document.getElementById('writing').innerHTML = ''
+        }
+    });
+
+    // Execute whenever the form is submitted
+    $("#chat_form").submit(function(e) {
+        // don't allow the form to submit
+        e.preventDefault();
+
+        var val = $("#chatbox").val();
+
+        // send out the "chat" event with the textbox as the only argument
+        if (val){
+            socket.emit("chat", val);
+
+            $("#chatbox").val("");
+            socket.emit('writing', '')
+        }
+        else{
+            alert('ITS EMPETY!!!!!')
+        }
+    });
+
+    $("#join").click(function(e) {
+      socket.emit('join', 'test')
+    })
+
+
+    $("#chatbox").keyup(function() {
+        var val = $("#chatbox").val();
+        socket.emit('writing', val)
+    })
+});
